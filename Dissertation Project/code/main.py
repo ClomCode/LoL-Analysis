402213150122dc82b030a7b0d2baa187dce96962
ClomCode <107809843+ClomCode@users.noreply.github.com>
2022-06-19 20:35:02 +0100

    Add files via upload

---
+++ Dissertation Project/code/main.py
@@ -0,0 +1,10 @@
+# Dissertation Python Script
+
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+import seaborn as sns
+
+data_21 = pd.read_csv('C:/Users/Calum/Desktop/Useful/University/Business Analytics/Dissertation/'
+                      'Datasets/2021_LoL_esports_match_data_from_OraclesElixir_20220606.csv')
+
