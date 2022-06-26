3b4e71370deed01fed2984dc76a5f2b299aaabad
ClomCode <19calumpalmer98@gmail.com>
2022-06-26 20:03:42 +0100

    updates

--- code/main.py
+++ code/main.py
@@ -5,7 +5,8 @@ import pandas as pd
 import matplotlib as plt
 import seaborn as sns
 
-data_21 = pd.read_csv('C:/Users/Calum/Desktop/Useful/University/Business Analytics/Dissertation/'
-                      'Datasets/2021_LoL_esports_match_data_from_OraclesElixir_20220606.csv')
+data_21 = pd.read_csv('C:/Users/Calum/Desktop/Useful/University/Business Analytics/Dissertation/Datasets/2021_LoL_esports_match_data_from_OraclesElixir_20220606.csv')
 
-
+# Data exploration
+data_21.info()
+data_21.head(20)
