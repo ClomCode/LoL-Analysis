51d29c00d560123f40e68832e49c15ebdfb79aad
ClomCode <19calumpalmer98@gmail.com>
2022-06-26 22:34:53 +0100

    updates

--- code/main.py
+++ code/main.py
@@ -5,8 +5,12 @@ import pandas as pd
 import matplotlib as plt
 import seaborn as sns
 
-data_21 = pd.read_csv('C:/Users/Calum/Desktop/Useful/University/Business Analytics/Dissertation/Datasets/2021_LoL_esports_match_data_from_OraclesElixir_20220606.csv')
+data_21 = pd.read_csv(
+    'C:/Users/Calum/Desktop/Useful/University/Business Analytics/Dissertation/Datasets/2021_LoL_esports_match_data_from_OraclesElixir_20220606.csv')
 
 # Data exploration
 data_21.info()
 data_21.head(20)
+# Just shy of 150k rows with 123 columns
+# Rows are a combination of Individual Player data and Team data
+
